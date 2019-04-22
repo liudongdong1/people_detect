1c2039f23b275c7fde68f6b895883c8735bc3740
liudongdong1 <3463264078@qq.com>
2019-04-22 17:10:21 +0800

    Add files via upload

---
+++ face_detect/facedetect.cpp
@@ -0,0 +1,209 @@
+#include "opencv2/objdetect.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+static void help()
+{
+    cout << "\nThis program demonstrates the use of cv::CascadeClassifier class to detect objects (Face + eyes). You can use Haar or LBP features.\n"
+            "This classifier can recognize many kinds of rigid objects, once the appropriate classifier is trained.\n"
+            "It's most known use is for faces.\n"
+            "Usage:\n"
+            "./facedetect [--cascade=<cascade_path> this is the primary trained classifier such as frontal face]\n"
+               "   [--scale=<image scale greater or equal to 1, try 1.3 for example>]\n"
+               "   [--try-flip]\n"
+               "   [filename|camera_index]\n\n"
+            "see facedetect.cmd for one call:\n"
+            "./facedetect --cascade=\"../haarcascade_frontalface_alt.xml\" --scale=1.3\n\n"
+            "During execution:\n\tHit any key to quit.\n"
+            "\tUsing OpenCV version " << CV_VERSION << "\n" << endl;
+}
+
+void detectAndDraw( Mat& img, CascadeClassifier& cascade,
+                    double scale, bool tryflip );
+
+string cascadeName;
+int main( int argc, const char** argv )
+{
+    VideoCapture capture;
+    Mat frame, image;
+    string inputName;
+    bool tryflip;
+    CascadeClassifier cascade;
+    double scale;
+
+    cv::CommandLineParser parser(argc, argv,
+        "{help h||}"
+        "{cascade|data/haarcascades/haarcascade_frontalface_alt.xml|}"
+        "{scale|1|}{try-flip||}{@filename||}"
+    );
+    if (parser.has("help"))
+    {
+        help();
+        return 0;
+    }
+    cascadeName = parser.get<string>("cascade");
+    scale = parser.get<double>("scale");
+    if (scale < 1)
+        scale = 1;
+    tryflip = parser.has("try-flip");
+    inputName = parser.get<string>("@filename"); //视频文件，或摄像头
+    if (!parser.check())
+    {
+        parser.printErrors();
+        return 0;
+    }
+    if (!cascade.load(samples::findFile(cascadeName)))
+    {
+        cerr << "ERROR: Could not load classifier cascade" << endl;
+        help();
+        return -1;
+    }
+    if( inputName.empty() || (isdigit(inputName[0]) && inputName.size() == 1) )
+    {
+        int camera = inputName.empty() ? 0 : inputName[0] - '0';
+        if(!capture.open(camera))
+        {
+            cout << "Capture from camera #" <<  camera << " didn't work" << endl;
+            return 1;
+        }
+    }
+    else if (!inputName.empty())
+    {
+        image = imread(samples::findFileOrKeep(inputName), IMREAD_COLOR);
+        if (image.empty())
+        {
+            if (!capture.open(samples::findFileOrKeep(inputName)))
+            {
+                cout << "Could not read " << inputName << endl;
+                return 1;
+            }
+        }
+    }
+    else
+    {
+        image = imread(samples::findFile("lena.jpg"), IMREAD_COLOR);
+        if (image.empty())
+        {
+            cout << "Couldn't read lena.jpg" << endl;
+            return 1;
+        }
+    }
+
+    if( capture.isOpened() )
+    {
+        cout << "Video capturing has been started ..." << endl;
+
+        for(;;)
+        {
+            capture >> frame;
+            if( frame.empty() )
+                break;
+
+            Mat frame1 = frame.clone();
+            detectAndDraw( frame1, cascade, scale, tryflip );
+
+            char c = (char)waitKey(10);
+            if( c == 27 || c == 'q' || c == 'Q' )
+                break;
+        }
+    }
+    else
+    {
+        cout << "Detecting face(s) in " << inputName << endl;
+        if( !image.empty() )
+        {
+            detectAndDraw( image, cascade, scale, tryflip );
+            waitKey(0);
+        }
+        else if( !inputName.empty() )
+        {
+            /* assume it is a text file containing the
+            list of the image filenames to be processed - one per line */
+            FILE* f = fopen( inputName.c_str(), "rt" );
+            if( f )
+            {
+                char buf[1000+1];
+                while( fgets( buf, 1000, f ) )
+                {
+                    int len = (int)strlen(buf);
+                    while( len > 0 && isspace(buf[len-1]) )
+                        len--;
+                    buf[len] = '\0';
+                    cout << "file " << buf << endl;
+                    image = imread( buf, 1 );
+                    if( !image.empty() )
+                    {
+                        detectAndDraw( image, cascade, scale, tryflip );
+                        char c = (char)waitKey(0);
+                        if( c == 27 || c == 'q' || c == 'Q' )
+                            break;
+                    }
+                    else
+                    {
+                        cerr << "Aw snap, couldn't read image " << buf << endl;
+                    }
+                }
+                fclose(f);
+            }
+        }
+    }
+
+    return 0;
+}
+
+void detectAndDraw( Mat& img, CascadeClassifier& cascade,
+                    double scale, bool tryflip )
+{
+    double t = 0;
+    vector<Rect> faces;
+    const static Scalar colors[] =
+    {
+        Scalar(255,0,0),
+        Scalar(255,128,0),
+        Scalar(255,255,0),
+        Scalar(0,255,0),
+        Scalar(0,128,255),
+        Scalar(0,255,255),
+        Scalar(0,0,255),
+        Scalar(255,0,255)
+    };
+    Mat gray, smallImg;
+
+    cvtColor( img, gray, COLOR_BGR2GRAY );
+    double fx = 1 / scale;
+    resize( gray, smallImg, Size(), fx, fx, INTER_LINEAR_EXACT );
+    equalizeHist( smallImg, smallImg ); //直方图均衡化，，用于提高图像的质量
+
+    t = (double)getTickCount();
+    cascade.detectMultiScale( smallImg, faces,
+        1.1, 2, 0
+        //|CASCADE_FIND_BIGGEST_OBJECT
+        //|CASCADE_DO_ROUGH_SEARCH
+        |CASCADE_SCALE_IMAGE,
+        Size(30, 30) );
+    t = (double)getTickCount() - t;
+    printf( "detection time = %g ms\n", t*1000/getTickFrequency());
+      for ( size_t i = 0; i < faces.size(); i++ )
+    {
+        Rect r = faces[i];
+        Mat smallImgROI;
+        Point center;
+        Scalar color = colors[i%8];
+        int radius;
+
+        double aspect_ratio = (double)r.width/r.height;
+        rectangle( img, Point(cvRound(r.x*scale), cvRound(r.y*scale)),
+                       Point(cvRound((r.x + r.width-1)*scale), cvRound((r.y + r.height-1)*scale)),
+                       color, 3, 8, 0);
+         putText(img, "people detected",         //输入参数为图像、文本、位置、字体、大小、颜色数组、粗细
+               Point(r.x*scale, r.y),
+               FONT_HERSHEY_COMPLEX,1, // font face and scale
+               Scalar(220, 20, 60), // 
+               2, LINE_AA); // line thickness and type   
+   }
+    imshow( "result", img );
+}
